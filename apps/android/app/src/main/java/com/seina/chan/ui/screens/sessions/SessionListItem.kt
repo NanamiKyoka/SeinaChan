aaad3dacf26bc806dfed9076548e1f10492b9c5c
NanamiKyoka <NanamiKyoka@outlook.com>
2026-06-24 11:24:33 +0800

    fix(session): 补充缺失的 background 导入

--- apps/android/app/src/main/java/com/seina/chan/ui/screens/sessions/SessionListItem.kt
+++ apps/android/app/src/main/java/com/seina/chan/ui/screens/sessions/SessionListItem.kt
@@ -1,6 +1,7 @@
 package com.seina.chan.ui.screens.sessions
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
